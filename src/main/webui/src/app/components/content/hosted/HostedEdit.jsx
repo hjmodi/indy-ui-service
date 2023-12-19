341b255e5e20ff2b9325bfcad8450939c2f41616
Harsh Modi <hmodi@redhat.com>
2023-12-19 14:34:49 -0500

    implement hosted views
    Signed-off-by: Harsh Modi <hmodi@redhat.com>

--- src/main/webui/src/app/components/content/hosted/HostedEdit.jsx
+++ src/main/webui/src/app/components/content/hosted/HostedEdit.jsx
@@ -14,8 +14,350 @@
  * limitations under the License.
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useLocation, useParams} from 'react-router-dom';
+import {useForm} from 'react-hook-form';
+import {PropTypes} from 'prop-types';
+import {StoreEditControlPanel as EditControlPanel} from '../common/StoreControlPanels.jsx';
+import {DisableTimeoutHint, DurationHint, Hint} from '../common/Hints.jsx';
+import {PackageTypeSelect} from '../common/PackageTypeSelect.jsx';
+// import ViewJsonDebugger from './Debugger.jsx';
+import {Utils} from '#utils/AppUtils.js';
+import {TimeUtils} from '#utils/TimeUtils.js';
+import {IndyRest} from '#utils/RestClient.js';
+import {PATTERNS} from "../../ComponentConstants.js";
+
+const {storeRes, disableRes} = IndyRest;
+
+const CertificateSection = ({store, register}) => <div className="sub-fields">
+  {
+    store.useAuth &&
+    <div className="detail-field">
+      <label>Client Key Password:</label>
+      <input type="password" defaultValue={store.key_password} {...register("key_password")} /><Hint hintKey="client_key" />
+    </div>
+  }
+    <div className="fieldset two-col">
+      {
+        store.useAuth &&
+        <div className="left-col">
+          <div className="textarea-label">
+            <label>Client Key</label><span className="hint">(PEM Format)</span>
+          </div>
+          {
+            // 64 columns is the original PEM line-length spec
+          }
+          <textarea className="cert" cols="68" defaultValue={store.key_certificate_pem} {...register("key_certificate_pem")}>
+          </textarea>
+        </div>
+      }
+      <div className="right-col">
+        <div className="textarea-label">
+          <label>Server Certificate</label><span className="hint">(PEM Format)</span>
+        </div>
+        {
+          // 64 columns is the original PEM line-length spec
+        }
+        <textarea className="cert" cols="68" defaultValue={store.server_certificate_pem} {...register("server_certificate_pem")}>
+        </textarea>
+      </div>
+    </div>
+</div>;
+
+CertificateSection.propTypes = {
+  store: PropTypes.object.isRequired,
+  register: PropTypes.func
+};
 
 export default function HostedEdit() {
-  return <div>This is not implemented yet!</div>;
+  const [state, setState] = useState({
+    store: {},
+    storeView: {}
+  });
+  const [useProxy, setUseProxy] = useState(false);
+  const [useAuth, setUseAuth] = useState(false);
+  const [useX509, setUseX509] = useState(false);
+  const location = useLocation();
+  const {packageType, name} = useParams();
+  const {
+    register,
+    reset,
+    trigger,
+    handleSubmit,
+    formState: {errors}
+  } = useForm();
+
+  const path = location.pathname;
+  const mode = path.match(/.*\/new$/u)? 'new':'edit';
+  // Give a default packageType
+  let store = {"packageType": "maven", "type": "hosted"};
+  useEffect(()=>{
+    if(mode === 'edit'){
+      const fetchStore = async () =>{
+        // get Store data
+        const res = await storeRes.get(packageType, "hosted", name);
+        if (res.success){
+          const raw = res.result;
+          const storeView = Utils.cloneObj(raw);
+          storeView.disabled = raw.disabled === undefined ? false : raw.disabled;
+          storeView.useX509 = raw.server_certificate_pem || raw.key_certificate_pem;
+          storeView.useProxy = raw.proxy_host && true;
+          // eslint-disable-next-line no-extra-parens
+          storeView.useAuth = (storeView.useProxy && storeView.proxy_user) || storeView.user;
+          // get Store disablement data
+          const timeoutRes = await disableRes.getStoreTimeout(store.packageType, store.type, store.name);
+          const cloned = Utils.cloneObj(storeView);
+          if (timeoutRes.success){
+            const timeout = timeoutRes.result;
+            cloned.disableExpiration = timeout.expiration;
+          }else{
+            Utils.logMessage(`disable timeout getting failed! Error reason: ${timeoutRes.error.message}`);
+          }
+          // Change state and re-rendering
+          setState({
+            storeView: cloned,
+            store: raw
+          });
+          reset(raw);
+          setUseProxy(storeView.useProxy);
+          setUseAuth(storeView.useAuth);
+          setUseX509(storeView.useX509);
+        }else{
+          // TODO: find another way to do error handling
+          Utils.logMessage(`Failed to get store data. Error reason: ${res.error.status}->${res.error.message}`);
+        }
+      };
+
+      fetchStore();
+    }
+  }, [packageType, name, mode, reset]);
+
+  if (mode === 'edit'){
+    store = state.store;
+  }
+
+  const handleUseProxy = event=>{
+    setUseProxy(event.target.checked);
+  };
+  const handleUseAuth = event => {
+    setUseAuth(event.target.checked);
+  };
+  const handleUseX509 = event => {
+    setUseX509(event.target.checked);
+  };
+
+  const changelog = register("changelog");
+  return (
+    <form onSubmit={e => e.preventDefault()}>
+      <div className="control-panel">
+        <EditControlPanel mode={mode} store={store}
+         handleSubmit={handleSubmit} validate={trigger} changelog={changelog} />
+      </div>
+
+      <div className="content-panel">
+        <div className="fieldset-caption">Basics</div>
+        <div className="fieldset">
+          <div className="detail-field">
+            <label>Package Type:</label>
+            {
+              mode==='new'?
+              <React.Fragment>
+                <PackageTypeSelect register={register} formErrors={errors} />
+              </React.Fragment>:
+              <span className="key">{store.packageType}</span>
+            }
+          </div>
+          <div className="detail-field">
+            <label>Name:</label>
+            {
+              mode==='new'?
+              <span>
+                <input type="text" size="25" {...register("name", {required: true, maxLength: 50})}
+                />{' '}
+                {errors.name?.type === "required" && <span className="alert">Name is required</span>}
+                {errors.name?.type === "maxLength" && <span className="alert">Name&apos;s length should be less than 50</span>}
+              </span>:
+              <span className="key">{store.name}</span>
+            }
+          </div>
+
+          <div className="detail-field">
+            <input type="checkbox" defaultChecked={!store.disabled} {...register("enabled")}/>{' '}
+            <label>Enabled?</label>
+            {
+              store.disabled && store.disableExpiration &&
+              <span className="hint">Set to automatically re-enable at {TimeUtils.timestampToDateFormat(store.disableExpiration)}</span>
+            }
+          </div>
+          <div className="detail-field">
+            <input type="checkbox" defaultChecked={store.authoritative_index} {...register("authoritative_index")} />{' '}
+            <label>Authoritative content Index?</label>
+            <span className="hint">Make the content index authoritative to this repository</span>
+          </div>
+
+          <div className="sub-fields">
+            <div className="detail-field">
+              <label>Disable Timeout:</label>
+              <input type="number" defaultValue={store.disable_timeout}
+               {...register("disable_timeout", {min: -1, max: 999999999})}/>{' '}
+              {errors.disable_timeout && <span className="alert">Not a valid number</span>}<br />
+              <DisableTimeoutHint />
+            </div>
+          </div>
+
+          <div className="detail-field">
+            <label>Hosted URL:</label>
+            <input type="text" defaultValue={store.url} size="92"
+            {...register("url", {required: true, pattern: PATTERNS.URL})} />{' '}
+            {errors.url?.type==="required" && <span className="alert">Hosted URL is required</span>}
+            {errors.url?.type==="pattern" && <span className="alert">Not a valid URL</span>}
+          </div>
+
+          <div className="sub-fields">
+            <div className="detail-field">
+              <input type="checkbox" defaultChecked={store.is_passthrough}
+              {...register("is_passthrough")} />{' '}
+              <label>{"Don't Cache Content"}</label>
+              <Hint hintKey="passthrough"/>
+            </div>
+            {!store.is_passthrough && <React.Fragment>
+              <div className="detail-field">
+                <label>Content Cache Timeout:</label>
+                <input type="text" defaultValue={store.cache_timeout_seconds} {...register("cache_timeout_seconds")} />
+                <DurationHint />
+              </div>
+              <div className="detail-field">
+                <label>Metadata Cache Timeout:</label>
+                <input type="text" defaultValue={store.metadata_timeout_seconds} {...register("metadata_timeout_seconds")} />
+                <DurationHint>{'24h 36m 00s. Negative means never timeout, 0 means using default timeout by Indy settings.'}</DurationHint>
+              </div>
+            </React.Fragment>
+            }
+          </div>
+          {/* prefetch support has been disabled in new Indy.
+           <div className="sub-fields">
+            <div className="detail-field">
+              <label>Pre-fetching Priority:</label>
+              <input type="text" size="5" defaultValue={store.prefetch_priority} {...register("prefetch_priority"} />
+              <PrefetchHint />
+            </div>
+            <div className="detail-field">
+              <span><input type="checkbox" defaultChecked={store.prefetch_rescan} {...register("prefetch_rescan"} /></span>{' '}
+              <label>Allow Pre-fetching Rescan?</label>
+            </div>
+            <div className="detail-field">
+              <label>Pre-fetching Listing Type:</label>
+              <input type="radio" name="prefetch_listing_type" defaultChecked={store.prefetch_listing_type==="html"} defaultValue="html" {...register("prefetch_listing_type"}/> <span>html</span>{' '}
+              <input type="radio" name="prefetch_listing_type" defaultChecked={store.prefetch_listing_type==="koji"} defaultValue="koji" {...register("prefetch_listing_type"}/> <span>koji</span>
+            </div>
+          </div>*/}
+        </div>
+
+
+        <div className="fieldset-caption">Description</div>
+        <div className="fieldset">
+          <textarea rows="3" className="text-description" {...register("description")}>
+            {store.description}
+          </textarea>
+        </div>
+
+        <div className="fieldset-caption">Capabilities</div>
+        <div className="fieldset">
+          <div className="detail-field">
+            <span>
+              <input type="checkbox" defaultChecked={store.allow_releases} {...register("allow_releases")}/>
+            </span>{' '}
+            <label>Allow Releases</label>
+          </div>
+          <div className="detail-field">
+            <span>
+              <input type="checkbox" defaultChecked={store.allow_snapshots} {...register("allow_snapshots")}/>
+            </span>{' '}
+            <label>Allow Snapshots</label>
+          </div>
+        </div>
+
+        <div className="fieldset-caption">Hosted Access</div>
+        <div className="fieldset">
+          <div className="detail-field">
+            <label>Request Timeout:</label>
+            <input type="text" defaultValue={store.timeout_seconds} {...register("timeout_seconds")} />
+            <DurationHint />
+          </div>
+          <div className="detail-hint">
+            <Hint hintKey="request_timeout" />
+          </div>
+
+          {
+            // HTTP Proxy
+          }
+          <div className="detail-field">
+            <input type="checkbox" defaultChecked={useProxy} onChange={e=>handleUseProxy(e)} />{' '}
+            <label>Use Proxy?</label>
+          </div>
+          {
+            useProxy&&
+            <div className="sub-fields">
+              <div className="detail-field">
+                <label>Proxy Host:</label>
+                <input type="text" size="20" defaultValue={store.proxy_host} {...register("proxy_host")} />
+              </div>
+              <div className="detail-field">
+                <label>Proxy Port:</label>
+                <input type="text" size="6" defaultValue={store.proxy_port} {...register("proxy_port")} />
+              </div>
+            </div>
+          }
+          {
+            // X.509 / auth
+          }
+          <div className="detail-field">
+            <input type="checkbox" defaultChecked={useAuth} onChange={e=>handleUseAuth(e)} />{' '}
+            <label>Use Authentication?</label>
+          </div>
+          {
+            useAuth &&
+            <div className="sub-fields">
+              <div className="detail-field">
+                <label>Username:</label>
+                <input type="text" size="25" defaultValue={store.user} {...register("user")} />
+              </div>
+              <div className="detail-field">
+                <label>Password:</label>
+                <input type="password" size="25" defaultValue={store.password} {...register("password")} />
+              </div>
+              {
+                store.use_proxy && <React.Fragment>
+                <div className="detail-field">
+                  <label>Proxy Username:</label>
+                  <input type="text" size="20" defaultValue={store.proxy_user} {...register("proxy_user")} />
+                </div>
+                <div className="detail-field">
+                  <label>Proxy Password:</label>
+                  <input type="password" size="20" defaultValue={store.proxy_password} {...register("proxy_password")} />
+                </div>
+              </React.Fragment>
+              }
+            </div>
+          }
+          <div className="detail-field">
+            <input type="checkbox" defaultChecked={useX509} onChange={e=>handleUseX509(e)} />{' '}
+            <label>{`Use Custom X.509 Configuration?`}</label>
+          </div>
+          {
+            useX509 && <CertificateSection store={store} register={register} />
+          }
+        </div>
+      </div>
+      {
+        // <ViewJsonDebugger enableDebug={enableDebug} storeJson={storeJson} rawJson={rawJson}
+      }
+    </form>
+  );
 }
+
+HostedEdit.propTypes={
+  store: PropTypes.object,
+  location: PropTypes.object,
+  match: PropTypes.object
+};
--- src/main/webui/src/app/components/content/hosted/HostedView.jsx
+++ src/main/webui/src/app/components/content/hosted/HostedView.jsx
@@ -14,8 +14,218 @@
  * limitations under the License.
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+import {PropTypes} from 'prop-types';
+import {StoreViewControlPanel as ControlPanel} from '../common/StoreControlPanels.jsx';
+import {Hint, PasswordMask} from '../common/Hints.jsx';
+import {StoreViewBasicSection as BasicSection} from '../common/StoreBasicSections.jsx';
+import {StoreViewCapabilitiesSection} from '../common/StoreCapabilitiesSections.jsx';
+import {LoadingSpiner} from '../common/LoadingSpiner.jsx';
+// import ViewJsonDebugger from './Debugger.jsx';
+import {Filters} from '#utils/Filters.js';
+import {Utils} from '#utils/AppUtils.js';
+import {TimeUtils} from '#utils/TimeUtils.js';
+import {IndyRest} from '#utils/RestClient.js';
+
+const {storeRes, disableRes} = IndyRest;
+
+const HostedAccessSection = ({store})=> <div className="fieldset">
+  <div className="detail-field">
+    <label>Request Timeout:</label>
+    <span>{TimeUtils.secondsToDuration(store.timeout_seconds)}</span>
+    <span><Hint hintKey="request_timeout" /></span>
+  </div>
+
+  {
+    // HTTP Proxy
+  }
+  <div className="detail-field">
+    <span>{Filters.checkmark(store.useProxy)}</span>
+    <label>Use Proxy?</label>
+  </div>
+  {
+    store.useProxy &&
+
+      <div className="sub-fields">
+        <div className="detail-field">
+          <label>Proxy Host:</label>
+          <span>{store.proxy_host}</span>
+        </div>
+        <div className="detail-field">
+          <label>Proxy Port:</label>
+          <span>{store.proxy_port}</span>
+        </div>
+      </div>
+
+  }
+  {
+    // X.509 / auth
+  }
+  <div className="detail-field">
+    <span>{Filters.checkmark(store.useAuth)}</span>
+    <label>Use Authentication?</label>
+  </div>
+  {
+      store.useAuth &&
+        <div className="sub-fields">
+        {
+          store.user &&
+          <div className="detail-field">
+            <label>Username:</label>
+            <span>{store.user}</span>
+          </div>
+        }
+        {
+          store.password &&
+          <div className="detail-field">
+            <label>Password:</label>
+            <span><PasswordMask /></span>
+          </div>
+        }
+        {
+          store.useProxy && store.proxy_user &&
+          <div className="detail-field">
+            <label>Proxy Username:</label>
+            <span>{store.proxy_user}</span>
+          </div>
+        }
+        {
+          store.useProxy && store.proxy_password &&
+          <div className="detail-field">
+            <label>Proxy Password:</label>
+            <span><PasswordMask /></span>
+          </div>
+        }
+        </div>
+
+  }
+  <div className="detail-field">
+      <span>{Filters.checkmark(store.useX509)}</span>
+      <label>Use Custom X.509 Configuration?</label>
+  </div>
+  {
+    store.useX509 &&
+
+      <div className="sub-fields">
+      {
+        store.key_password &&
+        <div className="detail-field">
+          <label>Client Key Password:</label>
+          <span><ap-password-mask></ap-password-mask></span>
+        </div>
+      }
+        <div className="fieldset two-col">
+        {
+          store.key_certificate_pem &&
+          <div className="left-col">
+            <div className="textarea-label">
+              <label>Client Key</label><span className="hint">(PEM Format)</span>
+            </div>
+            {
+              // 64 columns is the original PEM line-length spec
+            }
+            <textarea readOnly className="cert" value={store.key_certificate_pem} />
+          </div>
+        }
+        {
+          store.server_certificate_pem &&
+          <div className="right-col">
+            <div className="textarea-label">
+              <label>Server Certificate</label><span className="hint">(PEM
+                Format)</span>
+            </div>
+            {
+              // 64 columns is the original PEM line-length spec
+            }
+            <textarea readOnly className="cert" value={store.server_certificate_pem} />
+          </div>
+        }
+        </div>
+      </div>
+
+  }
+</div>;
+
+HostedAccessSection.propTypes={
+  store: PropTypes.object.isRequired
+};
 
 export default function HostedView() {
-  return <div>This is not implemented yet!</div>;
+  const [state, setState] = useState({
+    store: {},
+    raw: {},
+    message: ''
+  });
+  const [loading, setLoading] = useState(true);
+  const {packageType, name} = useParams();
+
+  useEffect(()=>{
+    setLoading(true);
+    const fetchStore = async () => {
+      const res = await storeRes.get(packageType, "hosted",name);
+      if (res.success){
+        const raw = res.result;
+        const store = Utils.cloneObj(raw);
+        store.disabled = raw.disabled === undefined ? false : raw.disabled;
+        store.useX509 = raw.server_certificate_pem || raw.key_certificate_pem;
+        store.useProxy = raw.proxy_host && true;
+        store.useAuth = store.useProxy && store.proxy_user;
+        store.useAuth = store.useAuth || store.user;
+
+        // get Store disablement data
+        const timeoutRes = await disableRes.getStoreTimeout(store.packageType, store.type, store.name);
+        const newStore = Utils.cloneObj(store);
+        if(timeoutRes.success){
+          const timeoutData = timeoutRes.result;
+          newStore.disableExpiration = timeoutData.expiration;
+        }
+        // Change state and re-rendering
+        setState({
+          store: newStore
+        });
+      }else{
+        Utils.logMessage(`Failed to get store data. Error reason: ${res.error.status}->${res.error.message}`);
+      }
+      setLoading(false);
+    };
+
+    fetchStore();
+  }, [packageType, name]);
+
+  if (loading) {
+    return <LoadingSpiner />;
+  }
+
+  const store = state.store;
+  if(!Utils.isEmptyObj(store)) {
+    return (
+      <React.Fragment>
+        <div className="control-panel">
+          <ControlPanel store={store} />
+        </div>
+
+        <div className="content-panel">
+          <div className="fieldset-caption">Basics</div>
+          <BasicSection store={store} />
+
+          <div className="fieldset-caption">Description</div>
+          <div className="fieldset">
+            <div className="text-field">
+              <textarea readOnly className="text-description" value={store.description} />
+            </div>
+          </div>
+
+          <StoreViewCapabilitiesSection store={store} />
+
+          <div className="fieldset-caption">Hosted Access</div>
+          <HostedAccessSection store={store} />
+        </div>
+        {
+          // <ViewJsonDebugger enableDebug={false} storeJson={store} rawJson={raw} />
+        }
+      </React.Fragment>
+    );
+  }
+  return null;
 }
